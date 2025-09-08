2d50fd15da4ed0a10d87a3f6be5987acb61b4df7
hosseinmatinfar <matinfarhossein@gmail.com>
2025-09-08 10:26:06 +0200

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,5 +1,5 @@
 // api/ping.js
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  return res.status(200).json({ ok: true, time: Date.now() });
+  res.status(200).json({ ok: true, time: Date.now() });
 };
