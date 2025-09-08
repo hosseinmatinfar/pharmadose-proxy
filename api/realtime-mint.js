388e895bb6367b5a40359ed83e0084f26277b7b7
hosseinmatinfar <matinfarhossein@gmail.com>
2025-09-08 10:47:14 +0200

    Update realtime-mint.js

--- api/realtime-mint.js
+++ api/realtime-mint.js
@@ -1,4 +1,3 @@
-// api/realtime-mint.js
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'content-type, authorization');
